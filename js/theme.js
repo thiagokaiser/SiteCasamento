cba0f3bdad2016ef275106b50c6a10bd32fd4836
Thiago Kaiser <thiago.kaiser@hotmail.com>
2019-03-29 20:34:20 -0300

    tamanho slider

--- js/theme.js
+++ js/theme.js
@@ -88,7 +88,7 @@ function BannerVideoSlider () {
       },
       responsiveLevels:[2220,1183,975,751],
                 gridwidth:[1170,970,770,350],
-                gridheight:[850,850,850,700],
+                gridheight:[500,500,500,400],
                 shadow:0,
                 spinner:"off",
                 autoHeight:"off",
